1fe654bbc13a665131431b048733be052f133ce4
Alina Gasperino <alina.gasperino@gmail.com>
2017-03-06 15:48:41 -0500

    Added homepage, login & sign up forms

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const layouts      = require('express-ejs-layouts');
-const mongoose     = require('mongoose');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('./models/user.js');
 
 
-mongoose.connect('mongodb://localhost/module-2-project');
+dotenv.config();
+mongoose.connect("mongodb://localhost/passport-local");
+//mongoose.connect(process.env.MONGODB_URI);
 
-const app = express();
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// default value for title local
+app.set('layout', 'layout');
+app.use(expressLayouts);
 app.locals.title = 'SoundShelf';
 
 // uncomment after placing your favicon in /public
@@ -26,20 +39,69 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(layouts);
+app.use(session({
+  secret: 'SoundShelf Sessions',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
 
-const index = require('./routes/index');
+app.use(passport.initialize());
+app.use(passport.session());
+
+    // <input type="email" name="email" id="email-input">
+    //                             |
+    //                     ---------
+    //                     |
+    // { usernameField: 'email' },
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  (email, password, next) => {
+    User.findOne({ email: email }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (!user) {
+        next(null, false, { message: "Incorrect email" });
+      } else if (!bcrypt.compareSync(password, user.password)) {
+        next(null, false, { message: "Incorrect password" });
+      } else {
+        next(null, user);
+      }
+    });
+  }
+));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+    cb(null, user);
+  });
+});
+
+
 app.use('/', index);
+app.use('/users', users);
+
+const authRoutes = require('./routes/auth-routes.js');
+app.use('/', authRoutes);
+
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
